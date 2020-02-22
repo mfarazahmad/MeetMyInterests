95e03b5d6c60b2adf45d357de9c7fe785e2a591f
Faraz <farazfkm@gmail.com>
2020-02-22 18:27:35 -0500

    new

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,31 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const dotenv = require('.dotenv')
+const dotenv = require('dotenv')
 const parse_json = require('parse-json')
 
-const app = express.app()
+// Allows use of environment variables
+dotenv.config()
 
-app.use(cors)
-app.use(mongoose)
+const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 const port = 7222;
 
-const client = mongoose.Connection('127.0.0.1')
+// Connect to Database
+const dbURL = process.env.MONGO_URL
+mongoose.connect(dbURL, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
+mongoose.connection.on('error', () => console.log('An error occurred from the database'))
+mongoose.connection.once('open', () => console.log('Succesfully connected to the database'))
 
-app.get('/', ((req, res) => {
+app.route('/').get(((req, res) => {
     console.log('HEY!')
+    res.send('HEY')
 }))
 
-app.listen((port) => {
-    console.log(`Listening on port $("port")`)
+// Start Node server
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
 })
