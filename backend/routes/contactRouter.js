fa736d2bfc24cfe47e679dcf41526f20e97af09a
Faraz <farazfkm@gmail.com>
2020-02-22 20:25:25 -0500

    new

--- backend/routes/contactRouter.js
+++ backend/routes/contactRouter.js
@@ -1,4 +1,9 @@
 const router = require('express').Router()
+const Contact = require('../models/contact_model')
 
+
+router.route('/save').post((req, res) => {
+    const data = req.body.data
+})
 
 module.exports = router
