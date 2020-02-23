3cb941e4b78bf9b2bc85975f8f6ccbbfedd314b9
Faraz <farazfkm@gmail.com>
2020-02-22 23:10:39 -0500

    new

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const path = require('path')
 
 // Allows use of environment variables
 require('dotenv').config()
@@ -10,8 +11,14 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'build')))
 
 const port = process.env.PORT | 7222;
+
+// Serve the React Frontend
+app.route('/').get((req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
+})
 
 // Connect to Database
 const dbURL = process.env.MONGO_URL
