e0f14a17ebeca2dd1ad56f2c974921f37dfcee33
Faraz Ahmad <farazahmad@Farazs-MacBook-Pro.local>
2023-07-30 23:30:12 -0400

    Updating Docker Files

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,4 +2,6 @@
 
 echo 'Deploying Service to Kubernetes Cluster'
 #kubectl apply -f deploy/deployment.yml
-helm upgrade --install --create-namespace testlocal --values ./deploy/values.yaml ./deploy/charts
+helm upgrade --install --create-namespace testlocal --values ./deploy/charts/values.yaml ./deploy/charts
+
+#helm install testkubernetes ./deploy/charts
