16e4abe0ecf4bc99aaf987d78ee58b2c1a3ca9c2
Faraz Ahmad <farazahmad@Farazs-MacBook-Pro.local>
2023-07-31 00:03:36 -0400

    Updating Helm Charts to Spin Up Pods

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 echo 'Deploying Service to Kubernetes Cluster'
-#kubectl apply -f deploy/deployment.yml
-helm upgrade --install --create-namespace testlocal --values ./deploy/charts/values.yaml ./deploy/charts
+
+# kubectl delete secret -n default regcredentials    
+# kubectl apply -f deploy/deployment.yml
+
+helm upgrade --install --create-namespace testkubernetes ./deploy/charts
 
 #helm install testkubernetes ./deploy/charts
+
+# Debug Mode
+#helm install testkubernetes ./deploy/charts --debug
+
+# helm upgrade --install --create-namespace testkubernetes --values ./deploy/charts/values.yaml ./deploy/charts
+
